37533ab6390be589e75799a1ddf3d9a0806f8d7a
KuhnLuisMatias <matiaskuhn.93@gmail.com>
2023-12-08 10:40:47 -0300

    Injector, Signal

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,56 +1,139 @@
-import { Component, signal } from '@angular/core';
+import {
+  Component,
+  Injector,
+  computed,
+  effect,
+  signal,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../../models/task.model';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-
 export class HomeComponent {
   tasks = signal<Task[]>([
     {
       id: Date.now(),
-      title:'Crear proyecto',
-      completed: false
-    }]);
+      title: 'Crear proyecto',
+      completed: false,
+    },
+  ]);
 
-  ChangeHandler(event: Event){
-    const input = event.target as HTMLInputElement;
-    const newTask = input.value;
-    this.AddTask(newTask);
+  ngOnInit() {
+    const storage = localStorage.getItem('tasks');
+    if (storage) {
+      const tasksLocalStorage = JSON.parse(storage);
+      this.tasks.set(tasksLocalStorage);
+    }
+    this.TrackTasks();
   }
 
-  DeleteTask(index: number){
-    this.tasks.update((tasks)=> tasks.filter((task,position) => position !== index));
+  TrackTasks() {
+    effect(
+      () => {
+        const tasks = this.tasks();
+        console.log(tasks);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+      },
+      {
+        injector: this.injector,
+      }
+    );
   }
-  
-  AddTask(task: string){
 
+  injector = inject(Injector);
+  filter = signal<'all' | 'pending' | 'completed'>('all');
+  tasksByFilter = computed(() => {
+    const filter = this.filter();
+    let tasks = this.tasks();
+    switch (filter) {
+      case 'pending':
+        tasks = tasks.filter((task) => !task.completed);
+        break;
+
+      case 'completed':
+        tasks = tasks.filter((task) => task.completed);
+        break;
+
+      default:
+        tasks = tasks;
+        break;
+    }
+    return tasks;
+  });
+
+  ChangeHandler() {
+    if (this.newTaskCtrl.valid) {
+      const value = this.newTaskCtrl.value;
+      this.AddTask(value);
+      this.newTaskCtrl.setValue('');
+    }
+  }
+
+  DeleteTask(index: number) {
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => position !== index)
+    );
+  }
+
+  AddTask(task: string) {
     const newTask = {
       id: Date.now(),
       title: task,
-      completed: false
-    }
+      completed: false,
+    };
 
-    this.tasks.update((tasks) => [...tasks,newTask]);
+    this.tasks.update((tasks) => [...tasks, newTask]);
   }
 
-  UpdateTask(index: number){
-    this.tasks.update((tasks)=>{
-      return tasks.map((task,position)=>{
-        if(position == index){
-          return{
+  UpdateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position == index) {
+          return {
             ...task,
-            completed: !task.completed
-          }
+            completed: !task.completed,
+          };
         }
         return task;
       });
     });
   }
 
+  UpdateTaskEditingMode(index: number) {
+    this.tasks.update((prevState) => {
+      return prevState.map((task, position) => {
+        if (position === index) return { ...task, editing: true };
+        return { ...task, editing: false };
+      });
+    });
+  }
+
+  UpdateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tasks.update((prevState) => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return { ...task, title: input.value, editing: false };
+        }
+        return task;
+      });
+    });
+  }
+
+  newTaskCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.pattern(/^\S*$/)],
+  });
+
+  changeFilter(filter: 'all' | 'pending' | 'completed') {
+    this.filter.set(filter);
+  }
 }
